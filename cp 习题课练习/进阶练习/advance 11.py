95bd72611cda318d5c0220bb775fa1f8fbbee84d
Administrator <1134876981@qq.com>
2019-04-26 17:11:35 +0800

    Add files via upload

--- cp 习题课练习/进阶练习/advance 11.py
+++ cp 习题课练习/进阶练习/advance 11.py
@@ -9,27 +9,24 @@
 学员小张查看了自己所报的课程
 学员小张在 查看了 自己在Python 3 的成绩列表然后退出了
 学院小张给了讲师小周好评"""
-
 Course_list = []
 
-
-class School(object):
-    def __init__(self, school_name):
+class School():
+    def __init__(self,school_name):
         self.school_name = school_name
-        self.students_list = []
-        self.teachers_list = []
-
+        self.Student_list = []
+        self.Teacher_list = []
         global Course_list
+    # hire(聘用)
+    def hire(self,obj):
+        self.Teacher_list.append(obj.name)
+        print("我们现在聘用了了一位新老师".format(obj.name))
+    # enroll(参加,注册)
+    def enroll(self,obj):
+        self.Student_list.append(obj.name)
+        print("我们又来了一位新学员".format(obj.name))
 
-    def hire(self, obj):
-        self.teachers_list.append(obj.name)
-        print("我们现在聘请一个新老师{}".format(obj.name))
-
-    def enroll(self, obj):
-        self.students_list.append(obj.name)
-        print("我们又有了一个新学员{}".format(obj.name))
-
-
+# grade(年级) grade_code(年级代码),grade_course(年级课程) members 成员
 class Grade(School):
     def __init__(self, school_name, grade_code, grade_course):
         super(Grade, self).__init__(school_name)
@@ -38,50 +35,47 @@ class Grade(School):
         self.members = []
         Course_list.append(self.course)
 
-        print("我们现在有了{}的{}的{}".format(self.school_name, self.code, self.course))
+        print("我们现在有了在{}学习{}年级{}课程的同学".format(self.school_name, self.code, self.course))
 
     def course_info(self):
-        print("课程大纲{} 是 day01，day02，day03".format(self.course))
+        print("课程大纲是{0}".format(self.course))
 
-
-Python = Grade("BJ", 3, 'Python')
-Linxu = Grade("CD", 1, "Linux")
-
-
-class School_member(object):
-    def __init__(self, name, age, sex, role):
+Python = Grade("电大",3,"python")
+Python.course_info()
+Linux = Grade("山大",4,"linux")
+Linux.course_info()
+# member成员
+class School_member():
+    def __init__(self,name,age,sex,role):
         self.name = name
         self.age = age
         self.sex = sex
         self.role = role
         self.course_list = []
 
-        print("我叫{}，我是一个{}".format(self.name, self.role))
+        print("我叫{},今年{}了,性别{},是一个{}"
+              .format(self.name,self.age,self.sex,self.role))
 
-
-stu_num_id = 00
-
-
+Stu_nun_id = 00
 class Students(School_member):
-    def __init__(self, name, age, sex, role, course):
-        super(Students, self).__init__(name, age, sex, role)
-        global stu_num_id
-        stu_num_id += 1
-        stu_id = course.school_name + "S" + str(course.code) + str(stu_num_id).zfill(2)
+    def __init__(self,name,age,sex,role,course):
+        super(Students, self).__init__(name,age,sex,role)
+        global Stu_nun_id
+        Stu_nun_id+=1
+        stu_id = course.school_name+"S"+str(course.code)+str(Stu_nun_id).zfill(2)
         # zfill 填充的作用，当只有一位数时前面填充0， 只能对str类型做操作
 
         self.id = stu_id
         self.mark_list = {}
 
-    def study(self, course):
-        print("我来这里学习{}课，我的学号是{}".format(course.course, self.id))
+    def study(self,course):
+        print("我来这里学{}课的,我的学号是{}".format(course.course,self.id))
 
-    def pay(self, course):
-        print("我交了1000块钱给{}".format(course.course))
-        self.course_list.append(course.course)
+    def pay(self,course):
+        print("我交了1000块钱学{}".format(course.course))
 
-    def praise(self, obj):
-        print("{}觉得{}课真棒".format(self.name, obj.name))
+    def praise(self,obj):
+        print("{}觉得{}课真棒".format(self.name,obj.name))
 
     def mark_check(self):
         for i in self.mark_list.items():
@@ -89,28 +83,45 @@ class Students(School_member):
 
     def out(self):
         print("我离开了")
+tea_nun_id = 00
+class Teacher(School_member):
+    def __init__(self,name,age,sex,role,course):
+        super(Teacher, self).__init__(name,age,sex,role)
+        global tea_nun_id
+        tea_nun_id+=1
 
+        tea_id = course.school_name+"T"+str(course.course)+str(tea_nun_id).zfill(2)
+        self.tea_id =tea_id
 
-tea_num_id = 00
+    def teac(self,course):
+        print("我是来{}学校讲{}课程的,我的id是{}".format(course.school_name,course.course,str(self.tea_id)))
 
+    def record_mark(self,Data,course,obj,level):
+        obj.mark_list["Data"+Data] = level
 
-class Teachers(School_member):
-    def __init__(self, name, age, sex, role, course):
-        super(Teachers, self).__init__(name, age, sex, role)
-        global tea_num_id
-        tea_num_id += 1
-        tea_id = course.school_name + "T" + str(course.code) + str(tea_num_id).zfill(2)
-        self.id = tea_id
-
-    def teach(self, course):
-        print("我来这里讲{}门课，我的id是{}".format(course.course, self.id))
-
-    def record_mark(self, Date, course, obj, level):
-        obj.mark_list["Day" + Date] = level
-
-
-a = Students("小张", 18, "M", "student", Python)
+a = Students("小王",18,"男","学生",Python)
 Python.enroll(a)
+a.study(Python)
+a.pay(Python)
 
+b = Students("小李",20,"女","学生",Linux)
+Linux.enroll(b)
+a.study(Linux)
+a.pay(Linux)
 
+t =Teacher("小周",30,"M","teacher",Python)
+Python.hire(t)
+t.teac(Python)
+t.record_mark('1',Python,a,"A")
+t.record_mark("1",Python,b,"B")
+t.record_mark('2',Python,a,"A")
+t.record_mark("2",Python,b,"A")
+print("小王查看了自己的课程")
+print(b.course_list)
+print("小王查看了自己的成绩")
+b.mark_check()
+print("小王退出了")
+b.out()
+print("给好评")
+a.praise(t)
 
