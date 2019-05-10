989e79e32979d476d635b1e17eca7725c147df02
Administrator <1134876981@qq.com>
2019-05-10 16:55:37 +0800

    Add files via upload

--- 02 高级语法系列/cp tkinter/tkinter 屏保.py
+++ 02 高级语法系列/cp tkinter/tkinter 屏保.py
@@ -13,3 +13,26 @@ Kinter项目实战-屏保
         - 颜色，大小，多少， 运动方向，变形等随机
         - 球能动，可以被调用
 """
+import random,tkinter
+class Ball():
+    def __init__(self,canvas,screenwidth,screenheight):
+        # 初始化屏幕的大小
+        self.screenwidth=screenwidth
+        self.screenheight=screenheight
+
+        # 且出现的初始位置的坐标
+        self.xpos = random.randint(10,int(screenwidth)-20)
+        self.ypos = random.randint(10,int(screenheight)-20)
+
+        # 定义球的运动速度
+        self.xvelocity = random.randint(4,20)
+        self.yvelocity = random.randint(4,20)
+
+        # 定义球的大小
+        # 球的大小由圆心减去半径
+        self.radius = random.randint(30,120)
+
+        # 定义颜色
+        # 颜色由rgb表示
+        c = lambda: random.randint(0,255)
+        self.color = '#%02x%02x%02x'%(c(), c(), c())
